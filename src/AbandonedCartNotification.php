b5224ba96143dd16c5a8d3a4606cbc732e887c84
Morven Lewis-Everley <morven@i-lateral.com>
2023-03-02 13:28:48 +0000

    Add ability for notification to apply to only carts and incomplete invoices

--- src/AbandonedCartNotification.php
+++ src/AbandonedCartNotification.php
@@ -2,18 +2,27 @@
 
 namespace SilverCommerce\Notifications\AbandonedCart;
 
+use DateTime;
 use StatusChangeRule;
-use SilverStripe\Core\ClassInfo;
 use SilverStripe\Forms\DropdownField;
-use SilverStripe\Core\Injector\Injector;
 use SilverCommerce\ShoppingCart\Model\ShoppingCart;
 use ilateral\SilverStripe\Notifier\Model\Notification;
 use ilateral\SilverStripe\Notifier\Model\NotificationRule;
 use ilateral\SilverStripe\Notifier\Types\NotificationType;
+use SilverCommerce\OrdersAdmin\Model\Invoice;
+use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\ORM\ArrayList;
 
 class AbandonedCartNotification extends Notification
 {
     private static $table_name = "Notifications_AbandonedCartNotification";
+
+    private static $carts_to_monitor = [
+        ShoppingCart::class,
+        Invoice::class => [
+            'filter' => ['Status' => 'incomplete']
+        ]
+    ];
 
     private static $disallow_rules = [
         NotificationRule::class,
@@ -24,18 +33,65 @@ class AbandonedCartNotification extends Notification
         NotificationType::class
     ];
 
-    public function compileSuitableBaseClasses(): array
+    /**
+     * Create a list of classnames as keys with possible
+     * filters (or empty arrays) as the values
+     */
+    public static function compileClassesAndFilters(): array
     {
-        $base_classes = ClassInfo::subclassesFor(ShoppingCart::class, true);
+        $classes = self::config()->get('carts_to_monitor');
         $return = [];
 
-        foreach (array_values($base_classes) as $classname) {
-            /** @var DataObject */
-            $obj = Injector::inst()->get($classname, true);
-            $return[$classname] = $obj->i18n_singular_name();
+        foreach($classes as $key => $value) {
+            if (is_array($value)) {
+                $class = $key;
+                $filter = $value;
+            } else {
+                $class = $value;
+                $filter = [];
+            }
+
+            if (isset($filter['filter'])) {
+                $filter = $filter['filter'];
+            }
+
+            $return[$class] = $filter;
         }
 
         return $return;
+    }
+
+    /**
+     * Manual selection of monitored object is disabled,
+     * as this is handled via seperate task
+     */
+    public function compileSuitableBaseClasses(): array
+    {
+        return [];
+    }
+
+    /**
+     * Filter out any classes with default filters and
+     * return a list of classnames to monitor
+     */
+    public function compileSuitableCartClasses(): array
+    {
+        $classes = self::compileClassesAndFilters();
+        return array_keys($classes);
+    }
+
+    /**
+     * Generate a custom list of monitored objects
+     */
+    public function getObjectType(): string
+    {
+        $classes = $this->compileSuitableCartClasses();
+
+        foreach($classes as $class) {
+            $types[] = singleton($class)->i18n_singular_name();
+        }
+
+        return implode(', ', $types);
     }
 
     public function getCMSFields()
@@ -45,10 +101,18 @@ class AbandonedCartNotification extends Notification
         $this->beforeUpdateCMSFields(
             function ($fields) use ($self) {
                 $fields->removeByName([
+                    'BaseClassName',
                     'StateCreated',
                     'StateUpdated',
                     'StateDeleted'
                 ]);
+
+                $fields->addFieldToTab(
+                    'Root.Main',
+                     ReadonlyField::create('ObjectType'),
+                     'Rules'
+                );
+                
 
                 /** @var FieldList $fields */
                 $fields->replaceField(
